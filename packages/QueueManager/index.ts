b20cf7027ec76e1bf9b70b4abccd1f0999ef34db
Haiyun Wang <why7121@gmail.com>
2022-07-17 11:23:19 +1000

    install dotenv inside QueManager

--- packages/QueueManager/index.ts
+++ packages/QueueManager/index.ts
@@ -1,7 +1,8 @@
-import dotenv from 'dotenv';
-dotenv.config();
 const fs = require('fs');
 import express,{ Request, Response } from 'express';
+import { resolve } from 'path';
+import dotenv from 'dotenv';
+dotenv.config({ path: resolve(__dirname, "../../.env") });
 
 const app = express();
 const port = process.env.PORT;
@@ -12,9 +13,9 @@ type sciName = {SciName:String}
 //get scientific name from input.json
 const inputData = JSON.parse(fs.readFileSync('./input.json',{encoding:'utf8', flag:'r'}));
 
-//send scientific name one by one to worker
+// send scientific name one by one to worker
 inputData.map(function(i:sciName){
-  app.get('/',(req,res)=>{
+  app.get('/',(req:Request,res:Response)=>{
     return res.status(200).send(i)
   })
 })
