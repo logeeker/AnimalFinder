bd32ba36e7f21275b77b038ed6547a452f2b72e9
Haiyun Wang <why7121@gmail.com>
2022-07-23 08:07:24 +1000

    remove redundant parameter name from getResultAfterSpellCorrection function in getResultAfterSpellCorrection.ts

--- packages/Worker/helpers/getResultAfterSpellCorrection.ts
+++ packages/Worker/helpers/getResultAfterSpellCorrection.ts
@@ -3,11 +3,8 @@ import { Page } from 'puppeteer';
 /**
  * @descritpion get result directly from result page
  * @param {Page} result page 
- * @param {string} sciName
  */
-async function getResultAfterSpellCorrection(page:Page,name:string) {
-    console.log('click do you mean link')
-  // await page.click('#mw-search-DYM-suggestion')
+async function getResultAfterSpellCorrection(page:Page){
   console.log('nav to search do you mean result page')
   try {
    await Promise.all([
